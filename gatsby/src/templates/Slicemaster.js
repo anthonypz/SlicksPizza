9a626c73861740563ff7ffd58c5f6071b4d21a43
Anthony P <43166263+anthonypz@users.noreply.github.com>
2022-03-27 20:26:44 -0700

    Remove console log

--- gatsby/src/templates/Slicemaster.js
+++ gatsby/src/templates/Slicemaster.js
@@ -1,10 +1,9 @@
-import { graphql } from 'gatsby';
-import React from 'react';
-import Img from 'gatsby-image';
-import SEO from '../components/SEO';
+import { graphql } from "gatsby";
+import React from "react";
+import Img from "gatsby-image";
+import SEO from "../components/SEO";
 
 export default function SlicemasterPage({ data: { person } }) {
-  console.log(person);
   return (
     <>
       <SEO title={person.name} image={person.image.asset.src} />
